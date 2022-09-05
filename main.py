771d29ac1de5a4ac363f244fb2ddc26da3c76deb
AngRotanak <angrotanak@gmail.com>
2022-09-06 02:32:09 +0700

    should be work

--- main.py
+++ main.py
@@ -5,20 +5,28 @@ from config import TOKEN, PORT
 
 
 def start(update, context):
-    update.message.reply_text("Hey hello {} from Heroku !!".format(update.message.from_user.username))
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
 
-def start(update, context):
-    update.message.reply_text(" hello {} ".format(update.message.from_user.username))
 
-if __name__ == '__main__':
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+def main():
     updater = Updater(TOKEN)
 
     dp = updater.dispatcher
 
-
     dp.add_handler(CommandHandler("help", help))
+
     dp.add_handler(CommandHandler("start", start))
+
     updater.start_polling()
-    updater.start_webhook("0.0.0.0", PORT, TOKEN, webhook_url='https://naktabot.herokuapp.com/'+TOKEN)
+    updater.start_webhook("0.0.0.0", PORT, TOKEN, webhook_url='https://naktabot.herokuapp.com/' + TOKEN)
     updater.idle()
+
+if __name__ == '__main__':
+    main()
+
 
