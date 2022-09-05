ba88ba6bf419985004dddb500a250d40e6c7eb86
AngRotanak <angrotanak@gmail.com>
2022-09-06 02:28:42 +0700

    should be work

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 from config import TOKEN, PORT
 
 
-def start(update:Updater, context:CallbackContext):
+def start(update, context):
     update.message.reply_text("Hey hello {} from Heroku !!".format(update.message.from_user.username))
 
-def start(update:Updater, context:CallbackContext):
+def start(update, context):
     update.message.reply_text(" hello {} ".format(update.message.from_user.username))
 
 if __name__ == '__main__':
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("start", start))
-
+    updater.start_polling()
     updater.start_webhook("0.0.0.0", PORT, TOKEN, webhook_url='https://naktabot.herokuapp.com/'+TOKEN)
     updater.idle()
 
