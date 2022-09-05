0210fb923c32cb2e1419b2996ec0b7788c230b61
AngRotanak <angrotanak@gmail.com>
2022-09-06 02:16:45 +0700

    should be work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 
 from config import TOKEN, PORT
 
@@ -7,13 +7,20 @@ from config import TOKEN, PORT
 def start(update:Updater, context:CallbackContext):
     update.message.reply_text("Hey hello {} from Heroku !!".format(update.message.from_user.username))
 
+def start(update:Updater, context:CallbackContext):
+    update.message.reply_text("Help {} from Heroku !!".format(update.message.from_user.username))
+
 if __name__ == '__main__':
     updater = Updater(TOKEN)
 
     dp = updater.dispatcher
 
+    # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
 
-    updater.start_webhook("0.0.0.0", PORT, TOKEN, webhook_url='https://naktabot.herokuapp.com/'+TOKEN)
+
+
+updater.start_webhook("0.0.0.0", PORT, TOKEN, webhook_url='https://naktabot.herokuapp.com/'+TOKEN)
     updater.idle()
 
